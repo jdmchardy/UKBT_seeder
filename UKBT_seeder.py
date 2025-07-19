d48942ea7fff0f9113e09544336ee18cb93b10f7
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2025-07-20 00:14:06 +0100

    Update UKBT_seeder.py

--- UKBT_seeder.py
+++ UKBT_seeder.py
@@ -269,6 +269,7 @@ def mk_pools(seeded_df, num_teams):
 
 # ---- Streamlit Interface ----
 def main():
+    st.image("UKBTlogo.png", width=150)
     st.title("UKBT Seeding App")
     st.subheader("Upload player list excel file to generate seeding.")
     st.markdown("A template for the player list required can be downloaded below.")
